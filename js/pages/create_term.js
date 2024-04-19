e0ebbb3ce6fac7a9fc45dc00bb64bd53e08ca268
Danilo Silva <contact@danilocgsilva.me>
2024-04-19 11:09:13 +0000

    Create form to create terms

--- js/pages/create_term.js
+++ js/pages/create_term.js
@@ -1,1 +1,36 @@
-export const createTermContent = "This is a create content page"
+export const createTermContent = `
+
+<div class="field is-horizontal m-2">
+  <div class="field-label">
+    <label class="label">New Term</label>
+  </div>
+  <div class="field-body">
+    <div class="field">
+      <div class="control">
+        <input class="input" type="text" placeholder="New Term Name" />
+      </div>
+    </div>
+    <div class="field">
+      <div class="control">
+        <input class="input" type="text" placeholder="Equivalent terms, with wall character separated. Eg.: direita | derecha" />
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="field is-horizontal m-2">
+  <div class="field-label">
+    <!-- some empty space -->
+  </div>
+
+  <div class="field-body">
+    <div class="field">
+      <div class="control">
+        <button class="button is-primary">Register</button>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+`

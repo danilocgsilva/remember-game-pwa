c8d62a93db668926d9a7ddd7d4a209e1e94691ff
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-04-23 01:56:49 +0000

    wip: implementing the data save

--- js/database.js
+++ js/database.js
@@ -3,7 +3,8 @@ import { createTermContent } from './pages/create_term.js'
 const contentAreaElement = document.getElementById('contentArea')
 
 let db
-const request = window.indexedDB.open("memorygame", 3);
+let objectStore
+const request = window.indexedDB.open("memorygame", 1);
 request.onerror = (event) => {
     alert("Got an error!!")
 }
@@ -15,11 +16,27 @@ request.onsuccess = (event) => {
     }
 }
 request.onupgradeneeded = (event) => {
-    const dbUpgrading = event.target.result
-    const objectStore = dbUpgrading.createObjectStore("name", {keyPath: "myKey"})
+    db = event.target.result
+    objectStore = db.createObjectStore("myObjectStore", { keyPath: "id" })
 }
 
 document.getElementById('createTerms').addEventListener("click", function () {
     contentAreaElement.innerHTML = createTermContent
-    
+    document.getElementById('save_term').addEventListener('submit', (event) => {
+        event.preventDefault()
+        
+        let newTerm = event.target.elements.namedItem('new_term').value
+        let equivalent = event.target.elements.namedItem('term_equivalent').value
+
+        const firstData = [
+            {
+                key: newTerm,
+                equivalents: equivalent
+            }
+        ]
+
+        let transaction = db.transaction([""])
+
+        alert(`Adding stuffs: ${newTerm}, ${equivalent}`)
+    })
 })
--- js/pages/create_term.js
+++ js/pages/create_term.js
@@ -1,36 +1,37 @@
 export const createTermContent = `
 
-<div class="field is-horizontal m-2">
-  <div class="field-label">
-    <label class="label">New Term</label>
-  </div>
-  <div class="field-body">
-    <div class="field">
-      <div class="control">
-        <input class="input" type="text" placeholder="New Term Name" />
-      </div>
+<form id="save_term">
+  <div class="field is-horizontal m-2">
+    <div class="field-label">
+      <label class="label">New Term</label>
     </div>
-    <div class="field">
-      <div class="control">
-        <input class="input" type="text" placeholder="Equivalent terms, with wall character separated. Eg.: direita | derecha" />
+    <div class="field-body">
+      <div class="field">
+        <div class="control">
+          <input class="input" type="text" name="new_term" id="new_term" placeholder="New Term Name" />
+        </div>
       </div>
-    </div>
-  </div>
-</div>
-
-<div class="field is-horizontal m-2">
-  <div class="field-label">
-    <!-- some empty space -->
-  </div>
-
-  <div class="field-body">
-    <div class="field">
-      <div class="control">
-        <button id="add_term_button" class="button is-primary">Register</button>
+      <div class="field">
+        <div class="control">
+          <input class="input" type="text" name="term_equivalent" id="term_equivalent" placeholder="Equivalent terms, with wall character separated. Eg.: direita | derecha" />
+        </div>
       </div>
     </div>
   </div>
 
-</div>
+  <div class="field is-horizontal m-2">
+    <div class="field-label">
+      <!-- some empty space -->
+    </div>
 
+    <div class="field-body">
+      <div class="field">
+        <div class="control">
+          <button id="add_term_button" class="button is-primary">Register</button>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</form>
 `
